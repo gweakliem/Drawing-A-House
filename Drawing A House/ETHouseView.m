e7da16f5e0dfb8e5cfea9dc757d1f2ee5649f78d
Gordon Weakliem <gordon@vrbo.com>
2013-12-02 00:09:39 -0700

    Added window and a door

--- Drawing A House/ETHouseView.m
+++ Drawing A House/ETHouseView.m
@@ -26,16 +26,39 @@
 {
     UIBezierPath *bezierPath = [UIBezierPath bezierPath];
     bezierPath.lineWidth = 2.0;
-  
+    [[UIColor blackColor] setStroke];
+    
+    // Main house outline
     [bezierPath moveToPoint:CGPointMake(1/6.0 * self.bounds.size.width, 5/6.0 * self.bounds.size.height)];
     [bezierPath addLineToPoint:CGPointMake(1/6.0 * self.bounds.size.width, 1/2.0 * self.bounds.size.height)];
     [bezierPath addLineToPoint:CGPointMake(1/2.0 * self.bounds.size.width, 1/6.0*self.bounds.size.height)];
     [bezierPath addLineToPoint:CGPointMake(5/6.0 * self.bounds.size.width, 1/2.0*self.bounds.size.height)];
     [bezierPath addLineToPoint:CGPointMake(5/6.0 * self.bounds.size.width, 5/6.0*self.bounds.size.height)];
     [bezierPath closePath];
-    
-    [[UIColor blackColor] setStroke];
     [bezierPath stroke];
+    // color the house
+    [[UIColor yellowColor] setFill];
+    [bezierPath fill];
+    
+    // add window
+    UIBezierPath *windowPath = [UIBezierPath bezierPathWithRect:CGRectMake(1/4.0 * self.bounds.size.width, 7/12.0 * self.bounds.size.height, 1/6.0 * self.bounds.size.width, 1/6.0 * self.bounds.size.height)];
+    [[UIColor blueColor] setFill];
+    [windowPath fill];
+    [windowPath stroke];
+    
+    // add door
+    UIBezierPath *doorPath = [UIBezierPath bezierPathWithRect:CGRectMake(1/2.0 * self.bounds.size.width, 7/12.0 * self.bounds.size.height, 1/6.0 * self.bounds.size.width, 1/4.0 * self.bounds.size.height)];
+    [[UIColor redColor] setFill];
+    [doorPath fill];
+    [doorPath stroke];
+    
+
+    // add chimney
+    
+    
+    // change roof color
+    
+
 }
 
 
